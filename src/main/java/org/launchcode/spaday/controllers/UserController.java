2c65379799393c21ed501b8dc3d9eac03a6e4337
Melanie Gunn <mgunn589@gmail.com>
2021-09-16 20:38:14 -0500

    studio complete

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +15,20 @@ public class UserController {
     public String displayAddUserForm(){
         return "user/add";
     }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+
+//        //if(user.getPassword() == verify){
+        if(false){
+            model.addAttribute("username", user.getUsername());
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords Must Match");
+            model.addAttribute("aUsername", user.getUsername());
+            model.addAttribute("aEmail", user.getEmail());
+            return "user/add";
+        }
+    }
+
 }
