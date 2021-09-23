82664e9df2904ee8fa725565eb68a18e705420e3
Melanie Gunn <mgunn589@gmail.com>
2021-09-22 19:59:59 -0500

    updated validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,24 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message="Username is required.")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message="Email must be proper e-mail format.")
     private String email;
+
+    @NotBlank(message="Password is required.")
+    @Size(min=6, message="Password must be at least 6 characters long.")
     private String password;
+
+    @NotBlank(message="Verify is required.")
+    private String verify;
 
     public User() {
 
@@ -38,5 +53,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
     }
 }
